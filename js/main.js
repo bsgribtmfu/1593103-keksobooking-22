eee4de89a5c24108e6ae5e7438d3d7499b7a4c99
bsgribtmfu <bsgribtmfu@mailfence.com>
2021-02-06 14:11:50 +0100

    Нужно больше ф-й #3

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
-function randomInteger(min, max) {
-  if (min >= max || min < 0 || max < 0) return false;
-  let rand = Math.random() * (max - min + 1) + min;
+function getRandomInteger(min, max) {
+  if (min >= max || min < 0 || max < 0) {
+    return false;
+  }
+  const rand = Math.random() * (max - min + 1) + min;
   return Math.floor(rand);
 }
 
-randomInteger(2, 5);
+getRandomInteger(2, 5);
 
-function randomIntegerCount(min, max, count) {
+function getRandomNumber(min, max, quantity) {
   let rand;
-  if (min >= max || min < 0 || max < 0) return false;
+  if (min >= max || min < 0 || max < 0) {
+    return false;
+  }
   if (min % 1 === 0 && max % 1 === 0) { // проверка на целое число
     rand = Math.random() * (max - min + 1) + min;
   } else {
-    rand = Math.random() * (max - min) + min;
+    rand = Math.random() * (max - min) + min; // если число не целое используем другой алгоритм/формулу
   }
-  return rand.toFixed(count);
+  return rand.toFixed(quantity);
 }
 
-randomIntegerCount(1.2, 1.4, 2);
+getRandomNumber(1.2, 1.4, 2);
