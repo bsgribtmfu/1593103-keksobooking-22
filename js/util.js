a50864fabe14d6b2ed365c87e0fbe11f2aff7af8
bsgribtmfu <bsgribtmfu@mailfence.com>
2021-02-16 11:31:30 +0100

    Работа с модулями

---
+++ js/util.js
@@ -0,0 +1,21 @@
+function checkNumber(min, max) {
+  return (min >= max || min < 0 || max < 0);
+}
+
+function getRandomInteger(min, max) {
+  if (!checkNumber(min, max)) {
+    const rand = Math.random() * (max - min + 1) + min;
+    return Math.floor(rand);
+  }
+  return -1;
+}
+
+function getRandomFloat(min, max, quantity) {
+  if (!checkNumber(min, max)) {
+    const rand = Math.random() * (max - min) + min;
+    return rand.toFixed(quantity);
+  }
+  return -1;
+}
+
+export { getRandomInteger, getRandomFloat };
